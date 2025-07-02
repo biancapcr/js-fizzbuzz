037f04f4b0bed500d4ff3313a51890d4d6c68c14
biancapcr <biancacristina.pocora@gmail.com>
2025-07-02 16:49:27 +0200

    2.if n = fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 // 1. Ciclo per i numeri da 1 a 100
 for (let i = 1; i <= 100; i++){
-    
+    // - Se n è multiplo di 3 e di 5 → FizzBuzz
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    }
 }
